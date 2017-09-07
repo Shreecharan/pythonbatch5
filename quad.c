9fc5e3d3b9fad448dcb349f072846b3a80008efd
Shreecharan <shch16is@cmrit.ac.in>
2017-09-07 17:11:15 +0530

    Update quad.c

--- quad.c
+++ quad.c
@@ -1,6 +1,6 @@
 
 import math 
-def roots(a,b,c,d):
+def quad(a,b,c,d):
     global r1,r2
     if(d==0): 
       r1=(-b)/(2*a) 
@@ -14,12 +14,15 @@ def roots(a,b,c,d):
        r1=-b/2*a
        r2=math.sqrt(-d)/2*a
        return print('the roots are complex\n r1={0}+i{1} r2={1}-i{2}'.format(r1,r2,r1,r2))
-def read():
-    return float(input('enter coefficients\n')) 
+ 
+           
+
+           
+           float(input('enter coefficients\n')) 
 a=read(); 
 b=read(); 
 c=read(); 
-d=(b**2)-(4*a*c) 
+d=(b*b)-(4*a*c) 
 print('discriminant={0}'.format(d)) 
 r=roots(a,b,c,d);
 print(r)
